c4310870c427c91182c5dda7519fca35da7696e8
devild13700 <74012211+devild13700@users.noreply.github.com>
2023-04-11 00:10:51 +0530

    Add files via upload

---
+++ login.php
@@ -0,0 +1,34 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Registration Form</title>
+    <link  rel="stylesheet" href="style.css">
+</head>
+<body>
+    <h1>Registration Form</h1>
+    <form id="form">
+        <table id="tab">
+            <tr>
+                <td><label>Name: </label></td>
+                <td><input type="text" name="name" id="name" /><br></td>
+            </tr>
+        <tr>  
+        <td><label>Roll Number: </label></td>
+        <td><input type="text" name="roll" id="roll" /><br></td>
+        </tr>
+        <tr>
+    </table>
+    <div id="button">
+        <input type="submit" name="submit" id="submit" value="Login" />
+        <label><a href="/student">Register</a></label>
+        
+        
+    </div>
+    </form>
+</body>
+</html>
